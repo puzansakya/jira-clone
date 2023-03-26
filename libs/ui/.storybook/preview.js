fc2701a50a816cee2ab1edd5b614f7f6910b529d
Puzan sakya <pujan@rigonepal.com>
2023-03-26 21:58:03 +0545

    Initial: Configure storybook

---
+++ libs/ui/.storybook/preview.js
@@ -0,0 +1,13 @@
+// .storybook/preview.js
+
+export const parameters = {
+    backgrounds: {
+        default: 'light',
+        values: [
+            {
+                name: 'light',
+                value: '#ffffff',
+            },
+        ],
+    },
+};
